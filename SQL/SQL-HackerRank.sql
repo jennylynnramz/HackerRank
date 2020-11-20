666856f1fb7253bd5639d798979cf6f473b2bae5
Jennifer Ramsey <60010423+jennylynnramz@users.noreply.github.com>
2020-11-20 10:59:14 -0800

    HackerRank SQL Challenges

--- SQL/SQL-HackerRank.sql
+++ SQL/SQL-HackerRank.sql
@@ -7,7 +7,6 @@ WHERE COUNTRYCODE = 'USA'
 AND POPULATION > 100000;
 
 
-
 '''Revising the Select Query II
 https://www.hackerrank.com/challenges/revising-the-select-query-2/problem?h_r=next-challenge&h_v=zen'''
 
@@ -15,3 +14,18 @@ SELECT NAME
 FROM CITY
 WHERE POPULATION > 120000
 AND COUNTRYCODE = 'USA';
+
+
+'''Select All
+https://www.hackerrank.com/challenges/select-all-sql/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen'''
+
+SELECT *
+FROM CITY;
+
+
+'''Select by ID
+https://www.hackerrank.com/challenges/select-by-id/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen'''
+
+SELECT *
+FROM CITY
+WHERE ID = '1661';
