9a1dcc0f7030a0229732bb16e3121deb32667d6d
Jennifer Ramsey <60010423+jennylynnramz@users.noreply.github.com>
2020-11-29 13:52:41 -0800

    Javascript day 1 - hackerrank

---
+++ Javascript/10-Days-of-Javascript/Day1-Arithmetic-Operatos.js
@@ -0,0 +1,11 @@
+function getArea(length, width) {
+    let area = length * width;
+    
+    return area;
+}
+
+function getPerimeter(length, width) {
+    let perimeter = (length * 2) + (width * 2);
+        
+    return perimeter;
+}
