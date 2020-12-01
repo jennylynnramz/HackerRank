f984c27faeb70b97c12cd9313daf830e315ea2ae
Jennifer Ramsey <60010423+jennylynnramz@users.noreply.github.com>
2020-11-30 22:26:18 -0800

    Javascript 10 days HackerRank

---
+++ Javascript/10-Days-of-Javascript/Day1-Functions.js
@@ -0,0 +1,11 @@
+function factorial(num){
+    var result = 1
+    for (var i=1; i<num+1; i++){
+        // console.log(i)
+        result = result * i
+    }
+    return result
+} 
+
+
+//https://www.hackerrank.com/challenges/js10-function/problem
