67fe259cc2d7e84965c9c3656835c68a5c910962
Jennifer Ramsey <60010423+jennylynnramz@users.noreply.github.com>
2021-02-02 13:57:46 -0800

    leetcode activity python and javascript

---
+++ Leetcode/javascript/average-salary-excluding-the-minimum-and-maximum-salary.js
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/average-salary-excluding-the-minimum-and-maximum-salary/submissions/
+
+var average = function(salary) {
+    console.log(salary)
+    salary.sort((a, b) => b - a);
+    console.log(salary)
+
+
+    var salSum = 0
+    for (i = 1; i < salary.length -1; i++) {
+        console.log(salary[i])
+        salSum += salary[i]       
+    }
+    
+    console.log(salSum)
+    var salAvg = salSum / (salary.length -2)
+    console.log(salAvg)
+
+    return salAvg
+    
+};
---
+++ Leetcode/python/average-salary-excluding-the-minimum-and-maximum-salary.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/average-salary-excluding-the-minimum-and-maximum-salary/submissions/
+
+def average(salary):
+    print(salary)
+    salary.sort()
+    
+    salSum = 0
+    for i in range(1, len(salary)-1):
+        print(salary[i])
+        salSum += salary[i]
+    
+    print(salSum)
+    salAvg = salSum / (len(salary)-2)
+    
+    return salAvg
