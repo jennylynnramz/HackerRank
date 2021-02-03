12fdb6d0c1f3b889a9789e25b9a935baea1fe1d1
Jennifer Ramsey <60010423+jennylynnramz@users.noreply.github.com>
2021-02-03 13:47:14 -0800

    Leetcode activity python and javascript

---
+++ Leetcode/javascript/kids-with-the-greatest-number-of-candies.js
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/submissions/
+
+var kidsWithCandies = function(candies, extraCandies) {
+    var maxCandies = Math.max.apply(null, candies)
+    var candyList = []
+    for (i=0; i < candies.length; i++) {
+        if (candies[i] + extraCandies >= maxCandies){
+            candyList.push(true)
+        }
+        else {
+            candyList.push(false)
+        }
+    }    
+    return candyList
+    
+};
---
+++ Leetcode/python/kids-with-the-greatest-number-of-candies.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/submissions/
+
+def kidsWithCandies(candies, extraCandies):
+    maxCandies = max(candies)
+    candyList = []
+    for candy in candies:
+        if candy + extraCandies >= maxCandies:
+            candyList.append(True)
+        else:
+            candyList.append(False)
+            
+    return candyList
